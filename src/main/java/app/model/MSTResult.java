ff7bb3b38c523aee07ead37b2f8064ee00866bdb
riri-anvarbekova <241194@astanait.edu.kz>
2025-10-26 14:26:33 +0500

    initial commit

---
+++ src/main/java/app/model/MSTResult.java
@@ -0,0 +1,12 @@
+package app.model;
+import java.util.List;
+
+public class MSTResult {
+    public List<Edge> mstEdges;
+    public MSTMetrics metrics;
+
+    public MSTResult(List<Edge> mstEdges, MSTMetrics metrics) {
+        this.mstEdges = mstEdges;
+        this.metrics = metrics;
+    }
+}
